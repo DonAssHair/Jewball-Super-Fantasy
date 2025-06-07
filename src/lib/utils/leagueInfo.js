9003b159b9128c05861738b111fabbc0284fafc0
DonAssHair <viceverzagg@gmail.com>
2025-06-06 18:10:18 -0700

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,6 +1,6 @@
 /*   STEP 1   */
 export const leagueID = "1227750085366927360"; // your league ID
-export const leagueName = "Jewball Super Fantasy League"; // your league name
+export const leagueName = "Jewball Super Fantasy Football"; // your league name
 export const dues = 30; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
